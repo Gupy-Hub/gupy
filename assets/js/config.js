4dc1b2dcef7f751bb149c6b80df2032db771453b
Gupy Hub <pedrogabrielsilvaneto@gmail.com>
2025-12-31 18:08:43 -0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,3 @@
-const SB_URL = "https://glqrpsyjwozvislyxapj.supabase.co";
-const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdscXJwc3lqd296dmlzbHl4YXBqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyNTQyOTgsImV4cCI6MjA4MTgzMDI5OH0.etzcmIHgPcGC12HwZPTU2u0DLtJdF3XTBSYW38O7S-w";
+const SB_URL = "https://btzdlrjqdzisvyskskeb.supabase.co";
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0emRscmpxZHppc3Z5c2tza2ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMzIzNDEsImV4cCI6MjA4MjcwODM0MX0.k49GeqGXUP2c3wR9Jo0vJgTdYl1DZumi7s17sGArIQE";
 const _supabase = supabase.createClient(SB_URL, SB_KEY);
